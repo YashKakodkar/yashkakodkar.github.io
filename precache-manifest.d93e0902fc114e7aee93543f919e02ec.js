d33f399be5c124aadcca9451db8012df492362b6
Yash Kakodkar <33426213+YashKakodkar@users.noreply.github.com>
2020-09-22 15:24:10 -0500

    Updates

---
+++ precache-manifest.d93e0902fc114e7aee93543f919e02ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6063ce2901611ae03f1ef5a9c0c0e00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "524a1b2a125f06157108",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "606e5c67945e8a75d1a3",
+    "url": "/static/css/main.083b7425.chunk.css"
+  },
+  {
+    "revision": "524a1b2a125f06157108",
+    "url": "/static/js/2.ae115f5c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ae115f5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "606e5c67945e8a75d1a3",
+    "url": "/static/js/main.0b1bac21.chunk.js"
+  },
+  {
+    "revision": "fc4862b0bf9df83fc482",
+    "url": "/static/js/runtime-main.74a65b46.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
