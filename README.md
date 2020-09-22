efd8c93eb697b22468d9188c8540f173b7cf8de3
Yash Kakodkar <33426213+YashKakodkar@users.noreply.github.com>
2020-09-22 16:29:27 -0500

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-## Yash Kakodkar Personal Website
-Checkout my website in React at (https://yashkakodkar.github.io).
-More updates to follow!
---
+++ precache-manifest.8fdc62e5f91cce10d982734f8d1e9e92.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f492294364c9a1ba8235269acf774eb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "666b5d0ac02eed1402a8",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "282844ac6942a35706d2",
+    "url": "/static/css/main.083b7425.chunk.css"
+  },
+  {
+    "revision": "666b5d0ac02eed1402a8",
+    "url": "/static/js/2.85ec3754.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.85ec3754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "282844ac6942a35706d2",
+    "url": "/static/js/main.2627ce64.chunk.js"
+  },
+  {
+    "revision": "fc4862b0bf9df83fc482",
+    "url": "/static/js/runtime-main.74a65b46.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
